2c65b0de82a304eb5ce20c62515ac81fe94b5803
Matias Ifran <ifran2855@gmail.com>
2026-08-24 14:41:16 -0400

    Replace trivial app.test.js with a real test of GET /

--- backend/test/app.test.js
+++ backend/test/app.test.js
@@ -1,8 +1,13 @@
 const { expect } = require('chai');
+const request = require('supertest');
 const app = require('../src/app');
 
-describe('app', () => {
-  it('exports an Express application', () => {
-    expect(app).to.be.a('function');
+describe('GET /', () => {
+  it('responds with 200 and a status ok payload', async () => {
+    const response = await request(app).get('/');
+
+    expect(response.status).to.equal(200);
+    expect(response.headers['content-type']).to.match(/application\/json/);
+    expect(response.body).to.deep.equal({ status: 'ok' });
   });
 });
